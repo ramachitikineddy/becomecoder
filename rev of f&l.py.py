0ce83a644cf8d436e107bbe15bb694996a3b3939
ramachitikineddy <84132077+ramachitikineddy@users.noreply.github.com>
2021-05-13 19:38:25 +0530

    Add files via upload

---
+++ rev of f&l.py.py
@@ -0,0 +1,30 @@
+num=int(input())
+nn=0
+temp=num
+f=0
+l=0
+c=r=0
+k=0
+p=1
+while num:
+    r=num%10
+    num=num//10
+    c+=1
+num=temp#123
+k=c=c-1
+print(k)
+if num>0:
+    l=num%10#3
+    f=num//pow(10,c)#1
+while num:
+    r=num%10
+    num=num//10
+    if c==k:#4=4
+         r=f#3=1
+    elif c==0:
+         r=l#1=3
+    nn=nn+r*p#21+3*100=321
+    p=p*10
+    c-=1
+    
+print(nn)
